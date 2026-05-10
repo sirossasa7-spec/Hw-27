c9819ba2ff6cb449b6eb66a10c1c691ae109bb66
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-10 19:21:58 +0200

    ForeignPassport.cpp

--- Hw-27/ForeignPassport.cpp
+++ Hw-27/ForeignPassport.cpp
@@ -1,1 +1,28 @@
 #include "ForeignPassport.h"
+#include <iostream>
+
+ForeignPassport::ForeignPassport() {}
+
+ForeignPassport::ForeignPassport(string series, string number, string surname, string name,
+    string patronymic, string birthDate, string birthPlace,
+    string issueDate, string issuedBy, string registration)
+    : Passport(series, number, surname, name, patronymic,
+        birthDate, birthPlace, issueDate, issuedBy, registration) {
+}
+
+void ForeignPassport::addVisa(const Visa& visa) {
+    visas.push_back(visa);
+}
+
+void ForeignPassport::printVisas() const {
+    cout << "\nVisas:\n";
+    for (const auto& v : visas) {
+        v.printVisa();
+        cout << "----------------\n";
+    }
+}
+
+void ForeignPassport::printInfo() const {
+    Passport::printInfo();
+    printVisas();
+}
