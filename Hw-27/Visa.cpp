5e036ea98c20b0a82dacb8a950f9005d816e1fff
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-10 19:20:15 +0200

    Visa.cpp

--- Hw-27/Visa.cpp
+++ Hw-27/Visa.cpp
@@ -1,1 +1,13 @@
 #include "Visa.h"
+#include <iostream>
+
+Visa::Visa(string country, string openDate, string closeDate) {
+    this->country = country;
+    this->openDate = openDate;
+    this->closeDate = closeDate;
+}
+
+void Visa::printVisa() const {
+    cout << "Visa to: " << country << endl;
+    cout << "Open: " << openDate << " Close: " << closeDate << endl;
+}
